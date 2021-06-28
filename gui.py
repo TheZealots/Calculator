4846042393fe7a548cffe82d13df116316b3e485
wangu10 <86185196+wangu10@users.noreply.github.com>
2021-06-28 22:21:21 +0800

    first part gui
    def __init__(self) until self.label

--- gui.py
+++ gui.py
@@ -1,1 +1,18 @@
 # gui code
+import tkinter as tk
+
+
+
+class Kalkyu:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("375x500") # window size of calculator
+        self.window.resizable(True, True) # resizable in width and length
+        self.window.title("ZealotsCalc") # calculator name
+
+
+        self.equation = ""
+        self.answer = ""
+        self.display_frame = self.create_display_frame()
+
+        self.label = self.create_display_labels()
