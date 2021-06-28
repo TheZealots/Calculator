c5860b19b15cfca1f68ae9b74a6a6bd0a2db19a0
NoelleGwendolynDana <84621521+NoelleGwendolynDana@users.noreply.github.com>
2021-06-28 20:21:48 +0800

    Update calculator_logic.py

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,5 +1,14 @@
 ## calc logic
 
+  
+  def add_to_expression(self, value):
+        self.equation += str(value)
+        self.update_equation_label()
+
+    def append_operator(self, operator):
+        self.equation += str(operator)
+        self.update_equation_label()
+  
   def evaluate(self):   
         try:
             self.answer = str(eval(self.equation))
@@ -8,4 +17,7 @@
             self.answer = "Syntax Error!"
         finally:
             self.update_answer_label()
+            
+  
+          
   
