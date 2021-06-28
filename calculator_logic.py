a97a6fd749dc4507d16197c113ee724682e03999
NoelleGwendolynDana <84621521+NoelleGwendolynDana@users.noreply.github.com>
2021-06-28 20:08:37 +0800

    Update calculator_logic.py
    added evaluate function

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,1 +1,11 @@
 ## calc logic
+
+  def evaluate(self):   
+        try:
+            self.answer = str(eval(self.equation))
+            self.ans = self.answer
+        except Exception as e:
+            self.answer = "Syntax Error!"
+        finally:
+            self.update_answer_label()
+  
