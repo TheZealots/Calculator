220bda72933402a08391c9554b609ed001099278
NoelleGwendolynDana <84621521+NoelleGwendolynDana@users.noreply.github.com>
2021-06-28 20:58:08 +0800

    Update calculator_logic.py
    added create_special_buttons and clear function

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,15 +1,28 @@
 ## calc logic
 
-  
-  def add_to_expression(self, value):
+
+		def add_to_expression(self, value):
         self.equation += str(value)
         self.update_equation_label()
 
     def append_operator(self, operator):
         self.equation += str(operator)
         self.update_equation_label()
-  
-  def evaluate(self):   
+    
+    def create_special_buttons(self):
+        self.create_clear_button()
+        self.create_delete_button()
+        self.create_equal_button()
+        self.create_ans_button()
+
+    def clear(self):
+        self.equation = ""
+        self.ans = self.answer
+        self.answer = ""
+        self.update_equation_label()
+        self.update_answer_label()
+
+  	def evaluate(self):   
         try:
             self.answer = str(eval(self.equation))
             self.ans = self.answer
