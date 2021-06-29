b34c20ddf79eae932afda657cfdb073aece37633
Aima Bangay <84392704+AimaBangay@users.noreply.github.com>
2021-06-29 18:12:04 +0800

    Update calculator_logic.py

--- calculator_logic.py
+++ calculator_logic.py
@@ -8,12 +8,6 @@
     def append_operator(self, operator):
         self.equation += str(operator)
         self.update_equation_label()
-    
-    def create_special_buttons(self):
-        self.create_clear_button()
-        self.create_delete_button()
-        self.create_equal_button()
-        self.create_ans_button()
 
     def clear(self):
         self.equation = ""
