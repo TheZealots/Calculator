c375c1d2057bb09c5b809735aaf938bf9dd678e9
NoelleGwendolynDana <84621521+NoelleGwendolynDana@users.noreply.github.com>
2021-07-01 11:14:45 +0800

    Update calculator_logic.py
    added comments

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,21 +1,21 @@
 ## calc logic
 
-
+    # kani nga function kay kanang taga pindot ug button sa user kay madagdag siya didtos eqaution
     def add_to_expression(self, value):
         self.equation += str(value)
         self.update_equation_label()
-
+    # kani same sa anang sa babaw pero para sa operators (+, -, /, *)
     def append_operator(self, operator):
         self.equation += str(operator)
         self.update_equation_label()
-
+    # kini kay kanang pang delete pero as a whole, like limpyo jud ang whole calcu pero i-remember niya tung last nga answer nimo
     def clear(self):
         self.equation = ""
         self.ans = self.answer
         self.answer = ""
         self.update_equation_label()
         self.update_answer_label()
-    
+    # kani kay pang delete isa-isa, pero if gamiton nimo ni parehas sa clear (kanang para madelete tanan naas expression), i-remember pud niya imong last answer
     def delete(self):
         if len(self.equation) != 0:
             self.list1 = []
@@ -25,7 +25,7 @@
         else:
             self.ans = self.answer
 
-            
+    # kini kay tung pang solve na sa equation         
     def evaluate(self):   
         try:
             self.answer = str(eval(self.equation))
@@ -34,21 +34,21 @@
             self.answer = "Syntax Error!"
         finally:
             self.update_answer_label()
-     
+    # kini katong ibalik niya imong last answer para magamit nimo siya sa equation
     def ans(self):
         self.equation = self.equation + self.ans
         self.update_answer_label()
         self.update_equation_label()
-    
+    # pang - update sa answer
     def update_answer_label(self):
         self.answer_label.config(text=self.answer[:])
-    
+    # pang - update sa equation
     def update_equation_label(self):
         equation = self.equation
         for operator, symbol in self.operations.items():
             equation = equation.replace(operator, f' {symbol} ')
         self.equation_label.config(text = equation)
-        
+    # para mag run <33    
     def run(self):
         self.window.mainloop()
 
